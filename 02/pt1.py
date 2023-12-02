f2610a1c48aef98456610a69593c26020a46a23f
Kehvarl <Kehvarl@Kehvarl.com>
2023-12-01 22:29:50 -0700

    Completed Day 2 2668/2832

--- 02/pt1.py
+++ 02/pt1.py
@@ -1,11 +1,36 @@
 from collections import defaultdict
 from pprint import pprint
 
-with open("test.txt") as f:
+with open("input.txt") as f:
     content = f.readlines()
     #content = [int(x) for x in f.readlines()]
 
+max_color = {'red': 12,
+       'green': 13,
+       'blue': 14}
+
+valid_games = 0
+
+games = {}
 for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+    valid = True
+    g, rest = v1.strip().split(":")
+    g = g[5:]
+    games[g] = []
+    rest = rest.split(";")
+    for draw in rest:
+        round = {}
+        results = draw.split(",")
+        for cubes in results:
+            count, color = cubes.strip().split(" ")
+            round[color] = int(count)
+            if int(count) > max_color[color]:
+                valid = False
+        games[g].append(round)
+    if valid:
+        valid_games += int(g)
+
+print(valid_games)
+
+
+
--- 02/pt2.py
+++ 02/pt2.py
@@ -5,8 +5,29 @@ with open("input.txt") as f:
     content = f.readlines()
     #content = [int(x) for x in f.readlines()]
 
+max_color = {'red': 12,
+       'green': 13,
+       'blue': 14}
 
+valid_games = 0
+
+games = {}
 for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+    valid = True
+    g, rest = v1.strip().split(":")
+    g = g[5:]
+    min_color = {'red': 0, 'green': 0, 'blue': 0}
+
+    rest = rest.split(";")
+    for draw in rest:
+        results = draw.split(",")
+        for cubes in results:
+            count, color = cubes.strip().split(" ")
+            if int(count) > min_color[color]:
+                min_color[color] = int(count)
+    print(min_color)
+    games[g] = min_color['red'] * min_color['green'] * min_color['blue']
+
+print(games)
+print(sum(games.values()))
+
