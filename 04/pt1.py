30d084ca6a5a91cec906c0973d7b6d7d2b0e15a9
Kehvarl <Kehvarl@Kehvarl.com>
2023-12-03 21:58:19 -0700

    PRep for Day 04

--- 04/pt1.py
+++ 04/pt1.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from pprint import pprint
 
-with open("input.txt") as f:
+with open("test.txt") as f:
     content = f.readlines()
     #content = [int(x) for x in f.readlines()]
 
