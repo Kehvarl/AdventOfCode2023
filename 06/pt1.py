60682f44db5be8b4833ffa31f9fba7df3c83ef3b
Kehvarl <Kehvarl@Kehvarl.com>
2023-12-05 22:17:06 -0700

    Day 06 3598/2864

--- 06/pt1.py
+++ 06/pt1.py
@@ -5,7 +5,29 @@ with open("input.txt") as f:
     content = f.readlines()
     #content = [int(x) for x in f.readlines()]
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+_, times = content[0].split(':')
+times = times.split()
+_, dist = content[1].split(':')
+dist = dist.split()
+
+results = []
+for idx, time in enumerate(times):
+    d = int(dist[idx])
+    time = int(time)
+    spd = 0
+    delta_spd = 1
+    can_beat = 0
+    for ms in range(0, time+1):
+        total = (time-ms) * ms
+        print(time, d, spd, total)
+        if total > d:
+            can_beat += 1
+    results.append(can_beat)
+
+print(times)
+print(dist)
+print(results)
+prod = 1
+for r in results:
+    prod *= r
+print(prod)
--- 06/pt2.py
+++ 06/pt2.py
@@ -5,8 +5,34 @@ with open("input.txt") as f:
     content = f.readlines()
     #content = [int(x) for x in f.readlines()]
 
+_, times = content[0].split(':')
+times = times.split()
+_, dist = content[1].split(':')
+dist = dist.split()
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+time = ""
+for ch in content[0]:
+    if ch.isnumeric():
+        time += ch
+time = int(time)
+
+dist = ""
+for ch in content[1]:
+    if ch.isnumeric():
+        dist += ch
+dist = int(dist)
+
+
+spd = 0
+delta_spd = 1
+can_beat = 0
+for ms in range(0, time+1):
+    total = (time-ms) * ms
+    #print(time, dist, spd, total)
+    if total > dist:
+        can_beat += 1
+
+
+print(time)
+print(dist)
+print(can_beat)
