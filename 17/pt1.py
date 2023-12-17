95fb3edf42d602096c063465367d534329061f6f
Kehvarl <Kehvarl@Kehvarl.com>
2023-12-16 23:45:14 -0700

    Day 17 failing solutions

--- 17/pt1.py
+++ 17/pt1.py
@@ -1,11 +1,51 @@
-from collections import defaultdict
+from collections import defaultdict, deque
+from heapq import heappush, heappop
 from pprint import pprint
 
-with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+with open("test.txt") as f:
+    #content = f.readlines()
+    content = [x.strip() for x in f.readlines()]
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+width = len(content[0])
+height = len(content)
+grid = {}
+for y, v1 in enumerate(content):
+    for x, v2 in enumerate(v1):
+        grid[(x, y)] = int(v2)
+
+
+allowed_turns = {
+    (0, 0): [(0, 1), (0, -1), (1, 0), (-1, 0)],
+    (0, 1): [(0, 1), (1, 0), (-1, 0)],
+    (0, -1): [(0, -1), (1, 0), (-1, 0)],
+    (1, 0): [(1, 0), (0, 1), (0, -1)],
+    (-1, 0): [(-1, 0), (0, 1), (0, -1)],
+}
+
+
+start = (0, 0, 0, 0, -1)
+dist = {}
+bfs_queue = [(0, start)]
+goal = (width-1, height-1)
+while bfs_queue:
+    (cost, (x, y, vx, vy, last_turn)) = heappop(bfs_queue)
+
+    if (x, y, vx, vy) in dist:
+        continue
+    dist[(x, y, vx, vy)] = cost
+    for option in allowed_turns[(vx, vy)]:
+        turn = (1 if option != (vx, vy) else last_turn + 1)
+        isvalid = (turn <= 3)
+        ivx, ivy = option
+        if 0 <= x + ivx < width and 0 <= y + ivy < height and isvalid:
+            ihl = grid[(x + ivx, y + ivy)]
+            heappush(bfs_queue, (cost + ihl, (x + ivx, y + ivy, ivy, ivx, turn)))
+
+ans = 999999
+print(grid)
+for (x, y, vx, vy), cost in dist.items():
+    #print((x, y, vx, vy), cost)
+    if (x, y) == goal:
+        ans = min(ans, cost)
+print(ans)
+
