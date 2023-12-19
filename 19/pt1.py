d673454be09280dfcfbdee7a0c892282ea0a91ae
Kehvarl <Kehvarl@Kehvarl.com>
2023-12-18 20:26:14 -0700

    Day 19 setup

---
+++ 19/pt1.py
@@ -0,0 +1,10 @@
+from collections import defaultdict, deque
+from pprint import pprint
+
+with open("input.txt") as f:
+    #content = f.readlines()
+    content = [x.strip() for x in f.readlines()]
+
+for v1 in content:
+    for v2 in v1:
+        pass
